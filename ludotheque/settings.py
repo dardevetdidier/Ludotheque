1cd36fba6a0feb7f5e8030eef6eb4d510cea3040
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-05 14:16:10 +0100

    modify settings.py

--- ludotheque/settings.py
+++ ludotheque/settings.py
@@ -123,15 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "blog/static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static/'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
