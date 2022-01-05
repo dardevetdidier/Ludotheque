d23a9bc3c30123fa920163ebc7ab1a3cb1f4407a
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-05 22:15:47 +0100

    add static files

--- ludotheque/settings.py
+++ ludotheque/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
     'cloudinary',
     'blog',
 ]
@@ -139,9 +138,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static'),
-                    os.path.join(BASE_DIR, 'static')
-                    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
