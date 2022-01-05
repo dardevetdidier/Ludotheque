a597e6e86e7b8cd05113fac2477bd85e9025cbee
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-05 22:27:32 +0100

    modify settings.py

--- ludotheque/settings.py
+++ ludotheque/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
 
 import django_heroku
 from dotenv import load_dotenv
@@ -98,8 +97,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
