2fbb996024d1768a2eafd943577c794465ca9596
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-06 15:21:50 +0100

    modify settings

--- ludotheque/settings.py
+++ ludotheque/settings.py
@@ -188,6 +188,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
